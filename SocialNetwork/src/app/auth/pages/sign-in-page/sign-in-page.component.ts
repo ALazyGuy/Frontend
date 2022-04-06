e300fae2bf778027af539f162b062f7d78dc5733
Daniil Selin <daniil_selin@epam.com>
2022-04-07 00:05:46 +0300

    feat: Implemented sign-in design and loading animation

--- SocialNetwork/src/app/auth/pages/sign-in-page/sign-in-page.component.ts
+++ SocialNetwork/src/app/auth/pages/sign-in-page/sign-in-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
-  styleUrls: ['./sign-in-page.component.scss']
+  styleUrls: ['./sign-in-page.component.scss'],
 })
 export class SignInPageComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: boolean = false;
+  formEnabled: boolean = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(event: Event) {
+    event.preventDefault();
+    this.switchAnimation();
+  }
+
+  private switchAnimation(){
+    this.loggedIn = true;
+    setTimeout(() => this.formEnabled = true, 200);
   }
 
 }
