172a93a06f8f65449179cb4a90b134f0ef8a8c45
Daniil Selin <daniil_selin@epam.com>
2022-04-06 22:26:56 +0300

    feat: Implemented routing for auth pages

--- SocialNetwork/src/app/app.module.ts
+++ SocialNetwork/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
